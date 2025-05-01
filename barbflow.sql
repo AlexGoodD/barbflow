994c39c7ae069680d44b975a8d721cbad7341219
AlexGoodD <alejandrochavarb@gmail.com>
2025-05-01 11:53:27 -0600

    Archivo Barbflow.sql agregado

---
+++ barbflow.sql
@@ -0,0 +1,71 @@
+-- Creación de tablas
+
+-- USE `Barblow`; <- Cambia a tu base de datos y descomenta esta línea
+
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) DEFAULT NULL,
+  `apellido` varchar(60) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `confirmado` tinyint(1) DEFAULT NULL,
+  `token` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `servicios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) DEFAULT NULL,
+  `precio` decimal(5,2) DEFAULT NULL,
+  `duracion` int DEFAULT NULL,
+  `descripcion` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `citas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `usuarioId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuarioId` (`usuarioId`),
+  CONSTRAINT `citas_ibfk_1` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `citasServicios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `citaId` int DEFAULT NULL,
+  `servicioId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citaId` (`citaId`),
+  KEY `servicioId` (`servicioId`),
+  CONSTRAINT `citasservicios_ibfk_3` FOREIGN KEY (`citaId`) REFERENCES `citas` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `citasservicios_ibfk_4` FOREIGN KEY (`servicioId`) REFERENCES `servicios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb3;
+
+
+-- Inserción de datos
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `admin`, `confirmado`, `token`) VALUES
+(9, ' Juanm', 'De la torre', 'correo@correo.com', '$2y$10$9TTiKdZXQaUQaSbVKd7wPOucLusU8ebkv2h2IgqNjQXs.uLTW7CAq', '1234567890', 0, 1, '');
+
+INSERT INTO `servicios` (`id`, `nombre`, `precio`) VALUES
+(1, 'Corte de Cabello Mujer ACTUALIZADO', 120.00),
+(2, 'Corte de Cabello Hombre', 80.00),
+(3, 'Corte de Cabello Niño', 60.00),
+(4, 'Peinado Mujer', 80.00),
+(5, 'Peinado Hombre', 60.00),
+(6, 'Peinado Niño', 60.00),
+(7, 'Corte de Barba', 60.00),
+(8, 'Tinte Mujer', 300.00),
+(9, 'Uñas', 400.00),
+(10, 'Lavado de Cabello', 50.00),
+(11, 'Tratamiento Capilar', 150.00);
+
+INSERT INTO `citas` (`id`, `fecha`, `hora`, `usuarioId`) VALUES
+(22, '2023-11-29', '10:30:00', 9);
+
+INSERT INTO `citasServicios` (`id`, `citaId`, `servicioId`) VALUES
+(18, 22, 2);
